dfb37ba7bea0c37857da482c1aaf80259055fb47
Chinweike99 <innocentakwolu1@gmail.com>
2025-01-25 09:58:28 +0100

    facial recognition

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Face Recognition App</h1>
+    </div>
+  );
+}
+
+export default App;
